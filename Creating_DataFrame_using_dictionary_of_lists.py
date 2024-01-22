43fe6f2733d23409297a477ba423867879a7cb1b
naveen <yaparalanaveen9@gmail.com>
2024-01-22 14:41:27 +0530

    Initial commit

---
+++ Creating_DataFrame_using_dictionary_of_lists.py
@@ -0,0 +1,9 @@
+# importing pandas as pd
+import pandas as pd
+
+# dictionary of lists
+dict = {'category':["one","two","three"],'values':[10,20,30]}
+
+df = pd.DataFrame(dict)
+
+print(df)
---
+++ reveresestring.py
@@ -0,0 +1,12 @@
+def reverse(s):
+    str = ""
+    for i in s:
+        str = str + i
+    return str
+
+
+s = "Geeksforgeeks"
+print("The original string is : ", end="")
+print(s)
+print("The reversed string(using loops) is : ", end="")
+print(reverse(s))
